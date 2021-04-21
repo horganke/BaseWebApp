d309f36c027a4f491f3e3c4953dadd3d6c7fc7df
horgank <horgank@usf.edu>
2021-04-21 15:40:31 -0400

    openweather api experiment

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,27 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function () {
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+  $(".search-button").on("click", function(){
+    searchWeather();
+  });
+})
+
+function getWeather(searchQuery) {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=Imperial&appid="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+
+  $.ajax(url,{success: function (data){
+    console.log(data);
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function (error) {
+      $(".error-message").text("An error occurred")
+    }})
+
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
 }
--- project/start.js
+++ project/start.js
@@ -1,5 +1,7 @@
 var express = require('express');
+// use express
 var app = express();
+//create express app
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,15 +9,26 @@ app.use(express.static(__dirname));
 
 // views is directory for all template files
 app.set('views', __dirname + '/html');
+//html folder for app template files
+
 app.set('view engine', 'ejs');
+//ejs templating language
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+app.get('/project', function(request, response) {
+  response.render('pages/project');
+});
+//what to do with the app gets a request
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+//binds it to port
 
 
 // This file is what handles incoming requests and
